ce8e3f501317f3495be1ba66ec256641d2c8c848
dbayo <david.bayo.nieto@gmail.com>
2015-05-01 13:21:38 +0200

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
     resources :bills
   end
 
+  root 'clients#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
