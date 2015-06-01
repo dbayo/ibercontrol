e4b50daae340faa0cd8ae1f34aeb3c1355b9e194
dbayo <david.bayo.nieto@gmail.com>
2015-06-01 18:27:59 +0200

    Remove contracts

--- app/views/contracts/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @contract, :id, :plazos, :price, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,6 @@ Rails.application.routes.draw do
       end
     end
 
-    resources :contracts
     resources :bills
   end
 
