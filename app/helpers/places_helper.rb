0b2aa1571097c40a9a7cc770859041e289e7f363
David Bayo <david.bayo.nieto@gmail.com>
2015-10-03 14:27:48 +0200

    Add icons instead of true/false values

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,2 +1,17 @@
 module PlacesHelper
+  def boolean_to_icons(value)
+    if value
+      content_tag(:i, '', class: "fa fa-check text-success")
+    else
+      content_tag(:i, '', class: "fa fa-times text-danger")
+    end
+  end
+
+  def boolean_to_spanish(value)
+    if value
+      Si
+    else
+      No
+    end
+  end
 end
